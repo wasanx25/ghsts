7ce356c01917a730f587e68b881131c04b5206ec
wasanx25 <wasanx25@gmail.com>
2019-02-02 00:57:34 +0900

    Add sample read yml

--- main.go
+++ main.go
@@ -3,12 +3,32 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 
+	yaml "gopkg.in/yaml.v2"
 	"github.com/google/go-github/github"
 )
 
+type Setting struct {
+	Repos []string `yaml:"repos"`
+}
+
 func main() {
+	var setting Setting
+
+	buf, err := ioutil.ReadFile("./settings.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = yaml.Unmarshal(buf, &setting)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(setting.Repos)
+
 	client := github.NewClient(nil)
 	opt := &github.RepositoryListByOrgOptions{Type: "public"}
 	repos, _, err := client.Repositories.ListByOrg(context.Background(), "github", opt)
